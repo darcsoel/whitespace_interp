3cc5d350196d2b1045a87f0b12e0bb8f65a8d942
ross <rostikvunarchuk@gmail.com>
2022-10-16 21:43:33 +0300

    add unit tests for further development

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-"""
-
-This is a sample Python script.
-
-Press Shift+F10 to execute it or replace it with your code.
-Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.//
-
-"""
+# to help with debugging
+def unbleach(n: str) -> str:
+    return n.replace(" ", "s").replace("\t", "t").replace("\n", "n")
 
 
-def print_hi(name: str) -> None:
-    """Use a breakpoint in the code line below to debug your script."""
-    print(f"Hi, {name}...")  # Press Ctrl+F8 to toggle the breakpoint.
+# solution
+def whitespace(code: str, inp: str = "") -> str:
+    output: str = ""
+    # stack: list = []
+    # heap: dict = {}
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == "__main__":
-    print_hi("PyCharm")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    return output
--- tests.py
+++ tests.py
@@ -1,8 +1,54 @@
 """
 Unit tests
 """
+from main import whitespace
 
 
-def test_1() -> None:
-    """Dummy test to prevent exit code 5"""
-    assert True
+def test_positive_number1() -> None:
+    output = "   \t\n\t\n \t\n\n\n"
+    assert whitespace(output) == "1"
+
+
+def test_positive_number2() -> None:
+    output = "   \t \n\t\n \t\n\n\n"
+    assert whitespace(output) == "2"
+
+
+def test_positive_number3() -> None:
+    output = "   \t\t\n\t\n \t\n\n\n"
+    assert whitespace(output) == "3"
+
+
+def test_positive_number4() -> None:
+    output = "    \n\t\n \t\n\n\n"
+    assert whitespace(output) == "0"
+
+
+def test_negative_number1() -> None:
+    output = "  \t\t\n\t\n \t\n\n\n"
+    assert whitespace(output) == "-1"
+
+
+def test_negative_number2() -> None:
+    output = "  \t\t \n\t\n \t\n\n\n"
+    assert whitespace(output) == "-2"
+
+
+def test_negative_number3() -> None:
+    output = "  \t\t\t\n\t\n \t\n\n\n"
+    assert whitespace(output) == "-3"
+
+
+def test_label1() -> None:
+    output = "   \t     \t\n\t\n  \n\n\n"
+    assert whitespace(output) == "A"
+
+
+def test_label2() -> None:
+    output = "   \t    \t \n\t\n  \n\n\n"
+    assert whitespace(output) == "B"
+
+
+def test_label3() -> None:
+    output = "   \t    \t\t\n\t\n  \n\n\n"
+    assert whitespace(output) == "C"
