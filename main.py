7f157d7170348f82b3274fdbd95768fc5df510c6
Ross <rostikvunarchuk@gmail.com>
2023-01-01 22:15:05 +0200

    minor

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ def unbleach(n: str) -> str:
     return n.replace(" ", "s").replace("\t", "t").replace("\n", "n")
 
 
+imps = {"stack_manipulation": " ", "arithmetic": "\t ", "heap_access": "\t\t", "io": "\t\n", "flow_control": "\n"}
+
+numbers = {
+    "positive": r"\s[\s\t]*\n",
+    "negative": r"\t[\s\t]*\n",
+}
+
+labels = r"[\s\t]+\n"
+
+
 # solution
 def whitespace(code: str, inp: str = "") -> str:
     output: str = ""
-    # stack: list = []
-    # heap: dict = {}
+    stack: list = []
+    heap: dict = {}
 
     return output
